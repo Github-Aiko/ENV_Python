644802e317e1744aeec18c7b04571c82e9901506
Aiko <aikocute@icloud.com>
2022-12-20 00:16:43 +0700

    Update

--- env/linux.sh
+++ env/linux.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# install python 3.11.1 and pip
-sudo apt-get update
-sudo apt-get install python3.11
-sudo apt-get install python3-pip
+# check python installation
+if [ -x "$(command -v python3)" ]; then
+	echo "Python 3 is already installed."
+else
+	echo "Python 3 is not installed."
+	echo "Installing Python 3..."
+	sudo apt-get update
+	sudo apt-get install python3.11
+	sudo apt-get install python3-pip
+fi
 
 # install required packages
 pip install -r https://raw.githubusercontent.com/Github-Aiko/ENV_Python_AI/master/requirements.txt
--- env/macos.sh
+++ env/macos.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# install python 3.11.1 and pip
-brew update
-brew install python3
+# check python installation
+if [ -x "$(command -v python3)" ]; then
+	echo "Python 3 is already installed."
+else
+	echo "Python 3 is not installed."
+	echo "Installing Python 3..."
+	brew update
+	brew install python3
+fi
 
 # install required packages
 pip3 install -r https://raw.githubusercontent.com/Github-Aiko/ENV_Python_AI/master/requirements.txt
